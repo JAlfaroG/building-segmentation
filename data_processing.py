6cf5d315030d5e8b53406752435dea4376df67ca
Javi Alfaro <48742560+JAlfaroG@users.noreply.github.com>
2022-10-21 01:20:04 -0600

    Update data_processing.py
    Implement geodesic area calculation function and use it to create the building area variable

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,16 @@
 import geopandas as gpd
+from pyproj import Geod
+
+
+def get_geodesic_area(polygon):
+    # specify a named ellipsoid
+    geod = Geod(ellps="WGS84")
+    
+    # abs() is used to return only positive areas. 
+    # A negative area may be returned depending on the winding direction of the polygon.
+    area = abs(geod.geometry_area_perimeter(polygon)[0])
+    
+    return area
 
 
 def get_data(data_path: str) -> gpd.GeoDataFrame:
@@ -6,6 +18,9 @@ def get_data(data_path: str) -> gpd.GeoDataFrame:
     gdf = gpd.read_file(data_path)
     gdf.columns = gdf.columns.str.lower()
 
-    subset = gdf.sample(25000)
+    gdf["area_m2"] = gdf.geometry.apply(get_geodesic_area)
+    gdf["area_km2"] = gdf["area_m2"] / 1000
+
+    subset = gdf.sample(2500)
 
     return subset
