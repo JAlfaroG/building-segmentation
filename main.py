766c3a49d91bf8b0df67bdf2714c1cd58d762fa7
Javi Alfaro <48742560+JAlfaroG@users.noreply.github.com>
2022-10-24 09:03:41 -0600

    Update main.py
    Add sample argument and load isochrone functionality

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 import webbrowser
 
 from data_processing import get_data
-from create_aoi import get_aoi, get_building_counts
+from create_aoi import get_aoi, get_building_counts, load_aoi
 from create_map import get_map
 
 
@@ -14,18 +14,20 @@ def main():
     parser.add_argument('conf', type=str)
     parser.add_argument('--radius', type=int, help='Radius around POI in meters', default=1000)
     parser.add_argument('--browser', action='store_true', help='Flag to open the map in a new browser tab.')
+    parser.add_argument('--sample', type=float, help='Sample proportion.', default=0.5)
     args = parser.parse_args()
 
     conf = json.load(open(f'{args.conf}', 'r'))
 
-    subset = get_data(conf["data_path"])
+    subset = get_data(conf["data_path"], sample_proportion=args.sample)
 
     esa_poi = conf["esa_poi"]
+    esa_poi, esa_aoi = load_aoi(conf["esa_aoi_path"])
 
-    circle_poly = get_aoi(esa_poi, args.radius)
-    buildings_within_area = get_building_counts(circle_poly, subset)
+    # circle_poly = get_aoi(esa_poi, args.radius)
+    buildings_within_area = get_building_counts(esa_aoi, subset)
 
-    m = get_map(esa_poi, subset, buildings_within_area, circle_poly)
+    m = get_map(esa_poi, subset, buildings_within_area, esa_aoi)
 
     save_dir = './outputs'
     if not os.path.exists(save_dir):
